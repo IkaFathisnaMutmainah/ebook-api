e07e35623d7f733d55397de55b121e7cdadd0cb6
IkaFathisnaMutmainah <mutmainahika183@gmail.com>
2022-10-20 10:53:16 +0700

    03 migration and model

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,7 @@ class AuthController extends Controller
 {
     public function me(){
         return [
-            'NIS' => '3103120110',
+            'NIS' => 3103120110,
             'Nama' => 'Ika Fathisna Mutmainah',
             'Phone' => '082340208345',
             'Class' => 'XII RPL 4'
