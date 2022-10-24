99391ae8a82d1bfb05d67cf9d90458f5f70ebefa
IkaFathisnaMutmainah <mutmainahika183@gmail.com>
2022-10-24 15:24:17 +0700

    challenge author

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,13 +22,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/me', [AuthController::class, 'me']);
 
+
 //Route::get('/books', [BookController::class, 'index']);
 //Route::get('/books/{id}', [BookController::class, 'show']);
 //Route::post('/books', [BookController::class, 'store']);
 //Route::put('/books/{id}', [BookController::class, 'update']);
 //Route::delete('/books/{id}', [BookController::class, 'destroy']);
 
-Route::resource('books', BookController::class)-> except(
+//Route::get('/authors', [AuthorController::class, 'index']);
+//Route::get('/authors/{id}', [AuthorController::class, 'show']);
+//Route::post('/authors', [AuthorController::class, 'store']);
+//Route::put('/authors/{id}', [AuthorController::class, 'update']);
+//Route::delete('/authors/{id}', [AuthorController::class, 'destroy']);
+
+//Route::resource('books', BookController::class)-> except(
+    //['create', 'edit']
+Route::resource('authors', AuthorController::class)-> except(
     ['create', 'edit']
 );
 
